6a68112e13fb210664223626e2a0dc82a23c2515
vishvaa-vsk <kvishvaa6@gmail.com>
2026-06-18 18:19:50 +0530

    fix(fish): check for rustup and cachyos config existence before sourcing

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,5 +1,6 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
+if test -f /usr/share/cachyos-fish-config/cachyos-config.fish
+    source /usr/share/cachyos-fish-config/cachyos-config.fish
+end
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
